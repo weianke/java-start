39344943ca428713e9ed3b73e4e552f429426a51
anke <245783065@qq.com>
2026-03-28 15:08:12 +0800

    docs: 新增api文档

--- src/main/java/com/example/helloworld/controller/UserController.java
+++ src/main/java/com/example/helloworld/controller/UserController.java
@@ -1,30 +1,38 @@
 package com.example.helloworld.controller;
 
 import com.example.helloworld.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
+@Tag(name = "用户模块")
 public class UserController {
 
-    @GetMapping("/user/{id}")
+    @Operation(summary = "根据ID获取用户") // 接口说明
+    @GetMapping("/{id}")
     public String getUserId(@PathVariable int id) {
         System.out.println(id);
         return "根据ID获取信息";
     }
 
-    @PostMapping("/user/add")
-    public String save(User user) {
+    @Operation(summary = "添加用户") // 接口说明
+    @PostMapping("/add")
+    public String save(@RequestBody User user) {
         System.out.println(user);
         return "添加用户";
     }
 
-    @PutMapping("/user/update")
-    public String update(User user) {
+    @Operation(summary = "更新用户") // 接口说明
+    @PutMapping("/update")
+    public String update(@RequestBody User user) {
         System.out.println(user);
         return "更新用户";
     }
 
-    @DeleteMapping("/user/{id}")
+    @Operation(summary = "删除用户") // 接口说明
+    @DeleteMapping("/{id}")
     public String deleteById(@PathVariable int id) {
         System.out.println(id);
         return "根据ID删除用户";
--- src/main/java/com/example/helloworld/entity/User.java
+++ src/main/java/com/example/helloworld/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.helloworld.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
@@ -7,25 +8,30 @@ import lombok.Data;
  * 对应前端登录/注册请求参数
  */
 @Data // 自动生成 getter/setter/toString/equals/hashCode 等方法
+@Schema(description = "用户实体")
 public class User {
 
     /**
      * 用户名
      */
+    @Schema(description = "用户名")
     private String username;
 
     /**
      * 密码
      */
+    @Schema(description = "密码")
     private String password;
 
     /**
      * 验证码
      */
+    @Schema(description = "验证码")
     private String code;
 
     /**
      * 登录成功后返回的 Token
      */
+    @Schema(description = "Token")
     private String token;
 }
