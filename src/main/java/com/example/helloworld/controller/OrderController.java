1ebeade0061c6ffc23afe9299d1a2bfb1bfdefe4
anke <245783065@qq.com>
2026-03-31 13:57:56 +0800

    feat: 优化接口

--- src/main/java/com/example/helloworld/controller/OrderController.java
+++ src/main/java/com/example/helloworld/controller/OrderController.java
@@ -3,7 +3,9 @@ package com.example.helloworld.controller;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.example.helloworld.common.result.Result;
 import com.example.helloworld.entity.Order;
+import com.example.helloworld.entity.User;
 import com.example.helloworld.mapper.OrderMapper;
+import com.example.helloworld.mapper.UserMapper;
 import com.example.helloworld.util.PageUtil;
 import com.example.helloworld.vo.PageQuery;
 import io.swagger.v3.oas.annotations.Operation;
@@ -20,6 +22,9 @@ public class OrderController {
 
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
 
 
     // 返回所有订单
@@ -40,6 +45,25 @@ public class OrderController {
         );
     }
 
+    // ==================== 新增订单 ====================
+    @Operation(summary = "新增") // 接口说明
+    @PostMapping("/add")
+    public Result<Void> add(@RequestBody Order order) {
+        if (order.getUid() == null) {
+            return Result.error(500, "用户ID不能为空");
+        }
+
+        // 2. 校验用户是否存在（核心！）
+        User existUser = userMapper.selectById(order.getUid());
+        if (existUser == null) {
+            return Result.error(500, "用户不存在，无法创建订单");
+        }
+
+        // 3. 用户存在 → 插入订单
+        int rows = orderMapper.insert(order);
+        return rows > 0 ? Result.success() : Result.error(500, "新增订单失败");
+
+    }
 
     // 删除订单
     @Operation(summary = "删除订单") // 接口说明
