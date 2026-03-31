eeb86a7841393a2cb24e2ce857169a361e5bef63
anke <245783065@qq.com>
2026-03-31 10:41:12 +0800

    feat: 优化接口

--- src/main/java/com/example/helloworld/controller/OrderController.java
+++ src/main/java/com/example/helloworld/controller/OrderController.java
@@ -2,11 +2,11 @@ package com.example.helloworld.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.example.helloworld.common.result.Result;
-import com.example.helloworld.common.result.ResultCode;
 import com.example.helloworld.entity.Order;
 import com.example.helloworld.mapper.OrderMapper;
 import com.example.helloworld.util.PageUtil;
 import com.example.helloworld.vo.PageQuery;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +21,9 @@ public class OrderController {
     @Autowired
     private OrderMapper orderMapper;
 
+
     // 返回所有订单
+    @Operation(summary = "查询所有订单") // 接口说明
     @GetMapping("/findAll")
     public Result<List<Order>> findAll() {
         List<Order> orderList = orderMapper.selectAllOrdersAndUser();
@@ -29,6 +31,7 @@ public class OrderController {
     }
 
     // 分页查询
+    @Operation(summary = "分页查询") // 接口说明
     @PostMapping("/page")
     public Result<List<Order>> page(@RequestBody PageQuery query) {
         IPage<Order> page = PageUtil.page(query, orderMapper);
@@ -39,6 +42,7 @@ public class OrderController {
 
 
     // 删除订单
+    @Operation(summary = "删除订单") // 接口说明
     @PostMapping("/delete")
     public Result<Void> delete(@RequestBody Order order) {
         int rows = orderMapper.deleteById(order.getId());
@@ -46,6 +50,7 @@ public class OrderController {
     }
 
     // 更新订单
+    @Operation(summary = "更新") // 接口说明
     @PostMapping("/update")
     public Result<Void> update(@RequestBody Order order) {
         int rows = orderMapper.updateById(order);
